2d671639ff98392a27d18f7573f2a105290334bb
Camila <camila7araujocosta@gmail.com>
2026-04-03 17:54:58 -0300

    Alteração do commit

--- codigos9.py
+++
@@ -1,9 +0,0 @@
-#Algoritmo que Leia um valor
-a = input("Digite um valor: ")
-
-#Converta o valor para float
-a = float(a)
-
-#Mostre a metade do valor 
-print("A metade do valor é: ", a / 2)
-print("Fim do programa")
---
+++ codigos9e10.py
@@ -0,0 +1,20 @@
+#Algoritmo que Leia um valor
+a = input("Digite um valor: ")
+
+#Converta o valor para float
+a = float(a)
+
+#Mostre a metade do valor 
+print("A metade do valor é: ", a / 2)
+print("Fim do programa")
+
+
+#Algoritmo que  Leia um número
+a = float(input("Digite um valor: "))
+
+#Informe: "Está dentro do intervalo" se estiver entre 0 a 10, caso contrário, informe: "Está fora do intervalo"
+if (a >= 0 and a <= 10):
+    print("Dentro do intervalo")
+else:
+    print("Fora do intervalo")    
+print("Fim do programa")
