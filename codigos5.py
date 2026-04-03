ee44fa94d22bdcdd27711e2194a730824e7fb23b
Camila <camila7araujocosta@gmail.com>
2026-04-03 17:19:10 -0300

    Quinto commit

---
+++ codigos5.py
@@ -0,0 +1,9 @@
+#Leia um número como texto
+a = input("Digite um número: ")
+
+#Converta para inteiro
+a = int(a)
+
+#Mostre o triplo do número
+print("O triplo do número é: ", a * 3)
+print("Fim do programa")
