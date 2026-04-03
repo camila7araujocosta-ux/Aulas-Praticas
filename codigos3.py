d1742a3f42919f453511c0358de2ab06594a8cb8
Camila <camila7araujocosta@gmail.com>
2026-04-03 17:09:20 -0300

    Alterações

--- codigos3.py
+++ codigos3.py
@@ -1,5 +1,9 @@
-#Algoritmo que leia um valor e: Mostre o valor digitado; Mostre o tipo da variável usando type ()
+#Algoritmo que leia um valor 
 a = int(input("Digite um valor: "))
-print("O valor digitado foi: ", a)
+
+#Mostre o valor digitado
+print("O valor digitado foi:", a)
+
+#Mostre o tipo da variável usando type()
 print("O tipo da varável é:", type (a))
 print("Fim do programa")
