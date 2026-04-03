17106155e26325a874e90c406d01e24d3e0427db
Camila <camila7araujocosta@gmail.com>
2026-04-03 17:13:42 -0300

    Quarto commit

---
+++ codigos4.py
@@ -0,0 +1,9 @@
+#Leia um número 
+a = int(input("Digite um número: "))
+
+#Informe se o número é par ou impar
+if (a % 2 == 0):
+    print("O número é par")
+else:
+    print("O número é impar")
+print("Fim do programa")        
